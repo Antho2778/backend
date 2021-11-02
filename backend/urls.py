82fb69b392d1e4ca3dcb79f9bb422776ca799c88
Anthony Mordret <antho27540@gmail.com>
2021-11-02 15:40:28 +0100

    Integrate img

--- backend/urls.py
+++ backend/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from backweb import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'prestations', views.PrestationView, 'prestation')
@@ -26,4 +28,4 @@ router.register(r'reviews', views.ReviewView, 'review')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
